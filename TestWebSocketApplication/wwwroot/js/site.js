e24bc90ce5d30bb6a1cd5713cd85c279fdb35892
Carson <krazykopter@gmail.com>
2019-07-10 21:04:48 -0600

    CAN WE COMMIT!?
    Added cards & drag and drop functionality for general concept purposes. added a cute background. Super cute.

--- TestWebSocketApplication/wwwroot/js/site.js
+++ TestWebSocketApplication/wwwroot/js/site.js
@@ -1,7 +1,31 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+//--------------------------------
+//  Drag & Drop Logic For Cards   |
+//--------------------------------
+
+function allowDrop(ev)
+{
+    ev.preventDefault();
+}
+
+function drag(ev)
+{
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev)
+{
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.replaceWith(document.getElementById(data));
+    //Depending on where the card was set, we need to increment a counter to a players total
+}
+
+//------------------------------------------
+//         Connection Information           | 
+//------------------------------------------
 var connection = new signalR.HubConnectionBuilder()
     .withUrl("/chatHub")
     .build();
